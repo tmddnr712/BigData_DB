56118e1f9c5d860b90509b82ebfaf34b2989181b
tmddnr712 <nsu712@naver.com>
2021-08-02 09:57:20 +0900

    0730 project

--- RStudio/0730/0730.R
+++ RStudio/0730/0730.R
@@ -105,3 +105,115 @@ library(lattice)
 str(airquality)
 xyplot(ozone ~ wind, data=airquality)
 xyplot(ozone ~ wind | month, data=airquality)
+
+install.packages("ggplot2")
+library(ggplot2)
+coin<- c(2,2,0,1,1,1,2,2,3,4,1,3,2,3,3,1,2,2,1,2,2)
+
+coin.num = sort(unique(coin))
+coin.freq = rep(0, length(coin.num)) #repeat
+for(i in 1:length(coin.num)) { #인덱스는 1부터 내용은 0부터 시작 
+  coin.freq[i] <- length(coin[coin==i-1])
+}
+coin.rel = round(coin.freq/length(coin), 2) #상대도수 분포표
+coin.cum = cumsum(coin.rel) #누적 상대 도수 분포표
+
+coin.freq <- data.frame(coin.num, coin.freq)
+names(coin.freq)[2] <- paste("val")
+coin.freq$type=rep("freq", length(coin.num))
+coin.freq
+
+coin.rel = data.frame(coin.num, coin.rel)
+names(coin.rel)[2] <- paste("val")
+coin.rel$type=rep("rel", length(coin.num))
+coin.rel
+
+coin.cum = data.frame(coin.num, coin.cum)
+names(coin.cum)[2] <- paste("val")
+coin.cum$type = rep("cum", length(coin.num))
+coin.cum 
+
+
+#------------------------------------------------------#
+install.packages("dplyr")
+library(dplyr)
+install.packages("data.table")
+library(data.table)
+election <- read.csv("C:/Users/노승욱/DB/RStudio/0730/election_2012.csv", fileEncoding="UTF-8")
+class(election)
+election.dt <- data.table(election)
+class(election.dt)
+str(election.dt)
+head(election.dt)
+
+# 문제1
+election.dt = election.dt %>% select(cand_id, cand_nm, contbr_nm, contbr_occupation, contb_receipt_amt)
+
+# 문제2
+romney <- subset(election.dt, cand_nm=="Bachmann, Michelle")
+head(romney)
+obama <- subset(election.dt, cand_nm == "Obama, Barack")
+head(obama)
+
+test1 <- subset(election.dt, contb_receipt_amt==250)
+head(test1)
+
+#문제3
+romney_6000over <- subset(election.dt, contbr_nm=="Bachmann, Michelle" & contb_receipt_amt > 600)
+head(romney_6000over)
+obama_6000over <- subset(election.dt, contbr_nm=="Obama, Barack" & contb_receipt_amt > 600)
+head(obama_6000over)
+
+test1 <- subset(election.dt, contb_receipt_amt>250)
+head(test1)
+
+# 중복값제거
+#election.dt %>% duplicated()
+#duplicates <- election.dt %>% duplicated() %>% table()
+#election.dt %>% distinct()
+#election.dt %>% distinct(cand_id, .keep_all=TRUE)
+#
+
+library(dplyr)
+election <- read.csv("C:/Users/yoobi/Downloads/election_2012.csv")
+View(election)
+
+# 문제 1
+sample <- data.frame(election$cand_id,election$cand_nm,election$contbr_nm,election$contbr_occupation,election$contb_receipt_amt)
+
+#문제2
+romney<- subset(sample, sample$election.cand_nm=="Romney, Mitt")
+head(romney)
+obama<- subset(sample, sample$election.cand_nm=="Obama, Barack")
+head(obama)
+
+
+#문제 3
+romney_6000over <- subset(romney, romney$election.contb_receipt_amt>= 600)
+obama_6000over <- subset(obama, obama$election.contb_receipt_amt>=600)
+
+
+#문제 4
+romney2 <- subset(romney, romney$election.contbr_occupation != "" & 
+                        +                       romney$election.contbr_occupation != "-")
+obama2 <- subset(obama, obama$election.contbr_occupation != "" &
+                     +                      obama$election.contbr_occupation != "-")
+
+write.csv(romney2,"romney.csv",row.names = F)
+write.csv(obama2,"obama.csv", row.names = F)
+
+#문제 5
+romney3 <- read.csv("C:/Users/yoobi/Documents/romney.csv")
+obama3 <- read.csv("C:/Users/yoobi/Documents/obama.csv")
+romney3_temp <- subset(romney3, romney3$election.contbr_occupation=="RETIRED") 
+obama3_temp <- subset(obama3, obama3$election.contbr_occupation == "RETIRED")
+sum(romney3_temp$election.contb_receipt_amt)
+sum(obama3_temp$election.contb_receipt_amt)
+
+
+# 문제 6
+romney3_top3 <- romney3[order(-romney3$election.contb_receipt_amt),4:5] # 기본은 오름차순 -붙이면 내림차순됨
+head(romney3_top3,n=3)
+
+obama3_top3 <- obama3[order(-obama3$election.contb_receipt_amt),4:5]
+head(obama3_top3,n=3)
