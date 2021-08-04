3b75017cda4f0457a4076bfd806e0b8450361bba
tmddnr712 <nsu712@naver.com>
2021-08-04 17:00:15 +0900

    0804 정규분포

--- RStudio/0804/0804.R
+++ RStudio/0804/0804.R
@@ -49,4 +49,45 @@ head(data)
 x <- data$survey
 summary(x)
 table(X) #불만족:0, 만족:1
+install.packages("prettyR")
+library(prettyR)
+binom.test(c(14, 136), p=0.2) # 불만족 20%
+# 비율검정
+# 귀무가설 : 비율 차이가 나지 않는다.
+# 대립가설 : 비율 차이가 난다.
+
+x <- data$time
+head(x)
+summary(x) #na처리리
+mean(x, na.rm=T)
+x1 <- na.omit(x)
+mean(x1)
+shapiro.test(x1)
+
+t.test(x1, mu=5.2) # 일주일에 노동시간(40)
+t.test(x1, mu=5.2, alter="two.side", conf.lvel=0.95) #기본값이 양측검정, 신뢰구간 95%
+# 유의수준 0.05로 결정
+# 방향성이 있는 연구 가설
+t.test(x1, mu=5.2, alter="greater", conf.lvel=0.95)
+
+data2 <- read.csv("C:/Users/노승욱/Desktop/R자료/two_sample.csv", header=TRUE)
+data
+head(data)
+dataset <- data2[c('method', 'score')]
+table(dataset$method)
+
+(method1 <- subset(dataset, method==1))
+(method2 <- subset(dataset, method==2))
+
+(method1_score <- method1$score)
+(method2_score <- method2$score)
+
+var.test(method1_score, method2_score) # p-value = 0.3002 등분산성성
+t.test(method1_score, method2_score, var.equal = T)
+# p-value = 0.04029 두 집단에는 평균차이가 있다.
+# method1_score, method2_score 두 방법 중에서 방법1이 방법2보다 더 큰 경우만 고려하면 된다.
+# 방향성이 있는 연구 가설
+t.test(method1_score, method2_score, alter="greater", var.equal=T)
+
+
 
